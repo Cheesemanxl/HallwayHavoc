2c411eb28d5ec6d6e67ba16a88a59d1202d91789
Cody <owen1003@gmail.com>
2019-08-02 19:44:50 -0500

    Added Placeholder Sprites. Created character that always moves right and a camera that follows.

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public float speed = 1.0f;
+
+    private Vector3 pos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        pos += Vector3.right;
+        transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * speed);
+        pos = transform.position;
+    }
+}
